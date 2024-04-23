b381423c2d121987a569ecee33c598cea90c3502
Farhan Jafri <farhan@joinzorro.com>
2024-04-24 01:34:24 +0530

    changes

--- src/user-module/controllers/user.controller.ts
+++ src/user-module/controllers/user.controller.ts
@@ -1,8 +1,8 @@
 import { BadRequestException, Body, Controller, Post, UseInterceptors } from "@nestjs/common";
-import { UserLoginDto, UserSignUpDto } from "src/dto/user.dto";
+import { UserLoginDto, UserSignUpDto } from "../../dto/user.dto";
 import { UserService } from "../services/user.service";
 import { v4 as uuidv4 } from 'uuid';
-import { AppInterceptor } from "src/app.interceptor";
+import { AppInterceptor } from "../../app.interceptor";
 import { Public } from "../../decorators";
 
 
